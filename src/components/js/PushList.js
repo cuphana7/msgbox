4db3283a9dd1ea6d3739ce254bcaf27e52ed9d06
suun <cuphana7@gmail.com>
2018-11-18 20:41:30 +0900

    asdf

--- src/components/js/PushList.js
+++ src/components/js/PushList.js
@@ -14,72 +14,30 @@ export default class PushList extends ReactRefreshInfiniteTableView  {
         return this.props.dataSource !== nextProps.dataSource;
     }
     */
-    
-    
-    
 
     render() {
-
         var cells = (this.props.dataSource)?this.props.dataSource.map(function(item, index) {
-            return <li key={index}>
-                <strong className="tit">알파원카드 알림</strong>
-                <span className="date">18.08.29 | 18:26:32</span>
+            return <ul className="pushList payment" key={index}>
+                <li>
+                    <strong className="tit">알파원카드 알림</strong>
+                    <span className="date">18.08.29 | 18:26:32</span>
 
-                <div className="cont">
-                        {item.MSG}
-                </div>
+                    <div className="cont">
+                            {item.MSG}
+                    </div>
 
-                <div className="select">
-                    <label htmlFor="sel1_1">해당 알림 삭제하기</label>
-                    <input type="checkbox" id="sel1_1" name="" className="inp1" />
-                </div>
-            </li>
+                    <div className="select">
+                        <label htmlFor="sel1_1">해당 알림 삭제하기</label>
+                        <input type="checkbox" id="sel1_1" name="" className="inp1" />
+                    </div>
+                </li>
+            </ul>
         }) : [];
 
         return (
-            <ul className="pushList payment" onScroll={this.viewDidScroll}>
+            <div className="pushWrap" onScroll={this.viewDidScroll}>
                 {cells}
-                {/* 
-                <li>
-                    <span className="date">18.08.29 | 18:26:32</span>
-
-                    <div className="cont">
-                        <p>
-                            KB국민카드5*1* 승인<br />
-                            김*민<br />
-                            12,300원 일시불<br />
-                            08/29 18:26<br />
-                            올리브영 광화문점<br />
-                            누적 827,940원
-								</p>
-                    </div>
-
-                    <div className="select">
-                        <label htmlFor="sel1_2">해당 알림 삭제하기</label>
-                        <input type="checkbox" id="sel1_2" name="" className="inp1" />
-                    </div>
-                </li>
-                <li>
-                    <span className="date">18.08.29 | 18:26:32</span>
-
-                    <div className="cont">
-                        <p>
-                            KB국민체크 2*3*<br />
-                            김*민님<br />
-                            08/29 14:32<br />
-                            4,000원<br />
-                            미니스톱<br />
-                            잔여 523,480원
-								</p>
-                    </div>
-
-                    <div className="select">
-                        <label htmlFor="sel1_3">해당 알림 삭제하기</label>
-                        <input type="checkbox" id="sel1_3" name="" className="inp1" />
-                    </div>
-                </li>
-                 */}
-            </ul>
+            </div>
         );
     }
 }
