daa1f59145baa7b761594a66d38b8d031702563e
Your Name <you@example.com>
2018-11-24 20:46:44 +0900

    asdf

--- public/js/common.js
+++ public/js/common.js
@@ -11076,3 +11076,64 @@ var commonJs = commonJs || {};
 }(jQuery));
 
 
+//푸시 리스트 토글 UI
+$('.pushList .toggleUI').on('click',function(e) {
+    e.preventDefault();
+    $(this).toggleClass('up');
+
+    //PUSH내용 초기 세팅
+    var cont = $(this).closest('.btnToggle').siblings('.cont'),
+      contH = cont.children('p').outerHeight(),
+      duration = contH > 500 ? contH : 500;
+
+    if($(this).hasClass('up')) {
+      //PUSH내용 보임
+      cont.css({display:'block',maxHeight:'none',height:60})
+      
+      contH = cont.children('p').outerHeight();
+      duration = contH > 500 ? contH : 500;
+
+      cont.css({height: contH, 'transition-duration':duration + 'ms'});	
+    } else {
+      //PUSH내용 닫힘
+      cont.css({height: 60});
+      setTimeout(function() {
+        cont.css({display:'-webkit-box',maxHeight:60,height:'auto'});
+      }, duration)
+    }
+  });
+
+  //이벤트 리스트 토글 UI
+  $('.pushEvent .toggleUI').on('click',function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    
+    setTimeout(function() {
+      $this.toggleClass('up');
+    }, 400)
+
+    var move_num = $('.pushEvent').outerHeight() - 56;
+
+    if($(this).hasClass('up')) {
+      //하단 이벤트 영역 슬라이드업
+      $('.pushEvent').css({webkitTransform: 'translate3d(0, -' + move_num + 'px, 0)',transition: 'all 500ms cubic-bezier(0.250, 0.250, 0.540, 0.930)'})
+      $('.dim').fadeIn().closest('body').css({overflow:'hidden'});
+    } else {
+      //하단 이벤트 영역 슬라이드다운
+      $('.pushEvent').css({webkitTransform: 'translate3d(0, 0, 0)',transition: 'all 500ms cubic-bezier(0.250, 0.250, 0.540, 0.930)'})
+      $('.dim').fadeOut().closest('body').css({overflow:'auto'});;
+    }
+  });
+  
+  //하단 이벤트 영역 초기 세팅
+  $('.puchEventCont .eventList').css({height: $(window).outerHeight() - 281}).closest('.pushEvent').css({bottom: ($('.pushEvent').outerHeight() - 56) * -1})
+
+  //삭제버튼 클릭 시 UI
+  $('.delete').on('click', function() {
+    $('.pushArea').addClass('delete');
+  });
+
+  //상단 삭제 탭 닫을 때 UI
+  $('.pushDelete .close').on('click', function() {
+    $('.pushArea').removeClass('delete');
+  });
