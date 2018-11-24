d478c5557263f8e535984fc9154edb85aba2c222
Your Name <you@example.com>
2018-11-24 20:00:09 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -184,6 +184,7 @@ class App extends Component {
     return new Promise(function(resolve, reject) {
       window.kbmobile.push.callApi( self.state.api.url_messages, self.state.messagesReq,function(res){
         console.log("messages called to page="+self.state.messagesReq.PAGE);
+        console.log(res.toString());
         resolve(res);
       });
     });  
