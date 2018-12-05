ad10ac696df9953783d022a496daa15bf9386479
suun <si01094457837@gmail.com>
2018-12-06 00:14:58 +0900

    adf

--- src/components/js/PushMsg.js
+++ src/components/js/PushMsg.js
@@ -7,7 +7,7 @@ import $ from 'jquery'
  * 2. 확대 축소 UI
  * 3. 자세히 보기 UI
  */
-export default class PushMsg extends Component  {
+export default class PushMsg extends Component {
 
     constructor(props) {
         super(props)
@@ -21,36 +21,28 @@ export default class PushMsg extends Component  {
     render() {
         const self = this;
         const { msg, ext, msgid } = this.props;
-        const imageUrl = (img) => { return "https://img2.kbcard.com/msg/cxv/template/system/"+img; }
+        const imageUrl = (img) => { return "https://img2.kbcard.com/msg/cxv/template/system/" + img; }
 
-        const msgToTag = msg.split("\n").map(function(item, index){
+        const convert = (text) => {
+            var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+            var text1 = text.replace(exp, "<a href='$1'>$1</a>");
+            var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+            return text1.replace(exp2, '$1<a target="_blank" href="http://$2">$2</a>');
+        }
+
+        const msgToTag = msg.split("\n").map(function (item, index) {
             var rUrlRegex = /(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/gi;
             var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
             var url = item.match(rUrlRegex);
-            /*
-            renderText() {
-                let parts = this.props.text.split(re) // re is a matching regular expression
-                for (let i = 1; i < parts.length; i += 2) {
-                  parts[i] = <a key={'link' + i} href={parts[i]}>{parts[i]}</a>
-                }
-                return parts
-              }
-              render() {
-                let text = this.renderText()
-                return (
-                  <div className="some_text_class">{text}</div>
-                )
-              }
-            */
-            
+
             if (url != null)
-                return <React.Fragment key={index}>{item.substring(0, item.indexOf("http"))}<a href={url} className="linkStyle"> {url} </a><br/></React.Fragment>
-            else 
-                return <React.Fragment key={index}>{item}<br/></React.Fragment>
+                return <React.Fragment key={index}>{item.substring(0, item.indexOf("http"))}<a href={url} className="linkStyle"> {url} </a><br /></React.Fragment>
+            else
+                return <React.Fragment key={index}>{item}<br /></React.Fragment>
         });
 
         const replaceUrl = (url) => {
-            return url.replace("kbcardmain://openUrl","");
+            return url.replace("kbcardmain://openUrl", "");
         }
 
         const clickMsgOpen = (e) => {
@@ -62,17 +54,17 @@ export default class PushMsg extends Component  {
                 contH = cont.children('p').outerHeight(),
                 duration = contH > 500 ? contH : 500;
 
-            if($(thisEle).hasClass('up')) {
+            if ($(thisEle).hasClass('up')) {
                 //PUSH내용 보임
-                cont.css({display:'block',maxHeight:'none',height:60})
+                cont.css({ display: 'block', maxHeight: 'none', height: 60 })
                 contH = cont.children('p').outerHeight();
                 duration = contH > 500 ? contH : 500;
-                cont.css({height: contH, 'transition-duration':duration + 'ms'});	
+                cont.css({ height: contH, 'transition-duration': duration + 'ms' });
             } else {
                 //PUSH내용 닫힘
-                cont.css({height: 60});
-                setTimeout(function() {
-                cont.css({display:'-webkit-box',maxHeight:60,height:'auto'});
+                cont.css({ height: 60 });
+                setTimeout(function () {
+                    cont.css({ display: '-webkit-box', maxHeight: 60, height: 'auto' });
                 }, duration)
             }
             e.preventDefault();
@@ -80,34 +72,34 @@ export default class PushMsg extends Component  {
 
         return (
             <div id="checkboxes">
-                
+
                 {/* 이미지 배너 */}
-                {ext[0].value !== "" ? 
+                {ext[0].value !== "" ?
                     <span className="banner">
                         <img src={imageUrl(ext[0].value)} alt="" />
                     </span>
-                :""}
+                    : ""}
                 {/* 메시지 내용 */}
                 <div className="cont">
                     <p className="shortMsg">{msgToTag}</p>
                 </div>
 
                 {/* 이미지 펼치기 버튼 */}
-                { msg.split("\n").length > 6 ? 
+                {msg.split("\n").length > 6 ?
                     <div className="btnToggle"><a href="#kbcard" ref={this.eleMsgOpen} className="toggleUI" onClick={clickMsgOpen} ><span>이벤트 내용 펼쳐짐</span></a></div>
-                : ""}
+                    : ""}
 
                 {/*msgOpenBtn*/}
-                
+
                 {/* 링크 버튼 */}
-                {ext.length === 3 && ext[2].value !== "" ? 
-                <div className="eventBtn"><a href={replaceUrl(ext[2].value)} className="btnL btnWhite">자세히보기</a></div>
-                :""}
-                
+                {ext.length === 3 && ext[2].value !== "" ?
+                    <div className="eventBtn"><a href={replaceUrl(ext[2].value)} className="btnL btnWhite">자세히보기</a></div>
+                    : ""}
+
                 {/* 삭제 클릭시 보이는 UI */}
                 <div className="select">
-                    <label htmlFor={"push"+msgid}>해당 알림 삭제하기</label>
-                    <input type="checkbox" id={"push"+msgid} name={msgid} className="inp1" value={msgid} />
+                    <label htmlFor={"push" + msgid}>해당 알림 삭제하기</label>
+                    <input type="checkbox" id={"push" + msgid} name={msgid} className="inp1" value={msgid} />
                 </div>
 
             </div>
